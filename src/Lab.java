51e93ad1bc41e7904c88061d691ca92fad2534dc
seanconnolly2345 <seanconnolly@ufl.edu>
2017-09-26 11:47:36 -0400

    Small hello world program

---
+++ src/Lab.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Lab {
+
+        public static void main(String[] args){
+            Scanner in = new Scanner(System.in);
+            int a = in.nextInt();
+            if(a > 0){
+                System.out.println("Hello World");
+            }
+            if(a <= 0){
+                System.out.println("Hola Mundo");
+            }
+        }
+    }
+
+
